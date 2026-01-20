6be71dd62f29449cf97433e640c873377b5dae55
codedbyabir <abir.supreox@gmail.com>
2026-01-21 01:54:58 +0600

    added pricing section

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+// Mobile Menu Toggle Script
 var toggleOpen = document.getElementById('toggleOpen');
 var toggleClose = document.getElementById('toggleClose');
 var collapseMenu = document.getElementById('collapseMenu');
@@ -12,3 +13,33 @@ function handleClick() {
 
 toggleOpen.addEventListener('click', handleClick);
 toggleClose.addEventListener('click', handleClick);
+
+
+
+
+
+// Pricing Table Toggle Script
+ const monthlyBtn = document.getElementById('monthlyBtn');
+    const yearlyBtn = document.getElementById('yearlyBtn');
+    const monthlyContent = document.getElementById('monthlyContent');
+    const yearlyContent = document.getElementById('yearlyContent');
+
+    monthlyBtn.addEventListener('click', () => {
+      monthlyBtn.classList.add('bg-primary', 'text-white');
+
+      yearlyBtn.classList.remove('bg-primary', 'text-white');
+      yearlyBtn.classList.add('text-gray');
+
+      monthlyContent.classList.remove('hidden');
+      yearlyContent.classList.add('hidden');
+    });
+
+    yearlyBtn.addEventListener('click', () => {
+      yearlyBtn.classList.add('bg-primary', 'text-white');
+
+      monthlyBtn.classList.remove('bg-primary', 'text-white');
+      monthlyBtn.classList.add('text-gray');
+
+      yearlyContent.classList.remove('hidden');
+      monthlyContent.classList.add('hidden');
+    });
